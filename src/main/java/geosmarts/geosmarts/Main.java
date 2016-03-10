d41c00de2f2ef5039539f6ab04cf3a5a1f331ca1
Juri <Juri@10.112.18.73>
2016-03-10 14:05:19 +0200

    OK

--- src/main/java/geosmarts/geosmarts/Main.java
+++ src/main/java/geosmarts/geosmarts/Main.java
@@ -1,13 +1,40 @@
 package geosmarts.geosmarts;
 
+import Dao.AlueDao;
+import Dao.StringDao;
+import Dao.ViestiDao;
+import geosmarts.geosmarts.Tietokantataulut.Alue;
+import geosmarts.geosmarts.Tietokantataulut.Viesti;
+import java.sql.*;
+import java.sql.Statement;
 import java.util.*;
+import spark.ModelAndView;
+import static spark.Spark.*;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
         Database db = new Database();
-        
-    }
 
+        StringDao sd = new StringDao(db);
+        
+        
+        ArrayList<String> aaa = new ArrayList<String>(sd.AlueetYhtAika());
+        String sana = "";
+        
+        
+        for (String s : aaa) {
+            sana = sana + s + "\n";
+            
+        }
+
+        String hei = sana;
+        get("/hei", (req, res) -> {
+            return hei;
+        });
+
+        //////
+    }
 }
